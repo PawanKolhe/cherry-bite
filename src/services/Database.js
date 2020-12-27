7d5378f053a0f0d886b3cb4e0a52d0d517a0e649
Pawan Kolhe <contact@pawankolhe.com>
2020-12-27 16:37:27 +0530

    add backend env

--- src/services/Database.js
+++ src/services/Database.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_ENDPOINT = 'http://localhost:1337';
+const API_ENDPOINT = process.env.REACT_APP_BACKEND_ENDPOINT || 'http://localhost:1337';
 
 const getRecipesFromDB = async () => {
   const options = {
